3848a2c732c4b2d3629414982593537e5b547b0e
Ahmad1378 <61465556+Ahmad1378@users.noreply.github.com>
2025-02-14 15:04:39 +0330

    Delete BitPin directory

--- BitPin/Content/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from Content.views import ContentView, ScoreView
-
-app_name = 'content'
-
-urlpatterns = [
-    path('', ContentView.as_view(), name='list'),
-    path('score/', ScoreView.as_view(), name='score'),
-]
